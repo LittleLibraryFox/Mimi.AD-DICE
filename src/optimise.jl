29da46114ebf9ddbe3704600fb4f1222012a6637
Amila Pintul <166591633+LittleLibraryFox@users.noreply.github.com>
2024-06-21 15:57:32 +0200

    Update optimise.jl

--- src/optimise.jl
+++ src/optimise.jl
@@ -78,7 +78,7 @@ function construct_objective(m::Model, optimised_mitigation_saving::Array{Float6
     # update S (savings rate)
     update_param!(m, :neteconomy, :S, [Vector{Missing}(missing, backup_timesteps); optimised_mitigation_saving[n_objectives+1:2 .* n_objectives]])
     # update PROT (protection level)
-    update_param!(m, :damages, :PROT, [Vector{Missing}(missing, backup_timesteps); optimised_mitigation_saving[2 .* n_objectives:end]])
+    update_param!(m, :damages, :PROT, [Vector{Missing}(missing, backup_timesteps); optimised_mitigation_saving[2 .* n_objectives+1:end]])
     # re-build model to evaluate welfare effects
     run(m)
     return m[:welfare, :UTILITY]
